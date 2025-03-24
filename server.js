e03820689f8f02928cdb82ae56341455334082b4
harshithgande <156864163+harshithgande@users.noreply.github.com>
2025-03-23 19:43:14 -0700

    Commit changes that weren’t showing before

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
