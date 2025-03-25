0397ccf7cbc88048d44963cea02ea5c8a7e20bcd
harshithgande <156864163+harshithgande@users.noreply.github.com>
2025-03-24 23:30:23 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-/* eslint-disable no-undef */
-// filepath: c:\Users\harsh\OneDrive\Desktop\Soccer-For-Change-Web\server.js
 const path = require('path');
 const express = require('express');
 
 const app = express();
-const PORT = 80;
+const PORT = process.env.PORT || 8080;
 
-/*app.get('/', (req, res) => {
-  res.send('Hello World!');
-});*/
-
-// Serve static files from the 'dist' directory
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'dist')));
+
+// Log requests to static files
+app.use((req, res, next) => {
+  console.log(`Request URL: ${req.url}`);
+  next();
+});
 
 // Serve the main index.html file for any unknown routes
 app.get('*', (req, res) => {
